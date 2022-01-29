624ac5fe15050955dab006cdb8b3714097e47778
Zain-Yousuf <zainyousuf749@gmail.com>
2022-01-29 11:21:56 +0500

    my project

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p v-html="RowHtml"></p>
+    <h3>{{ name }}</h3>
+    <h3>{{ name }}</h3>
+    <h3>{{ ok ? "yes" : "No" }}</h3>
+    <button v-bind:disabled="disableBtn" v-on:click="updatetext">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      name: "zain",
+      a: 10,
+      b: 10,
+      ok: false,
+      RowHtml: "<h1>Hello zain</h1>",
+      disableBtn:true
+    };
+  },
+  methods: {
+    updatetext() {
+      this.name = "sufyan";
+      this.ok = !this.ok;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
